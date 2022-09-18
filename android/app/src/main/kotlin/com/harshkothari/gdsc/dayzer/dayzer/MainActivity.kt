03ec1d135d8395cc7529ce27718652ed4c030a89
HarshKothari88 <techsmash88@gmail.com>
2022-09-18 19:57:46 +0530

    new

---
+++ android/app/src/main/kotlin/com/harshkothari/gdsc/dayzer/dayzer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.harshkothari.gdsc.dayzer.dayzer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ build/app/generated/source/buildConfig/release/com/harshkothari/gdsc/dayzer/dayzer/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.harshkothari.gdsc.dayzer.dayzer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.harshkothari.gdsc.dayzer.dayzer";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
